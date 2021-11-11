50219edce287811a53dc8c28d4150f7bbf345c81
AWADREE <62517784+AWADREE@users.noreply.github.com>
2021-11-11 15:36:53 +0200

    added parallax
    added parallax background and foreground

---
+++ Stone Game/Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	private float length, startpos, ypos;
+	public GameObject cam;
+	public float parallexEffect;
+// Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        ypos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+        float ydist = (cam.transform.position.y * parallexEffect);
+
+        transform.position = new Vector3(startpos + dist, ypos + ydist, transform.position.z);
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+	}	
+}
