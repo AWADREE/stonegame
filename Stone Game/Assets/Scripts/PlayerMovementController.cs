570d5ce362bbd3f7b64eb4136c6b191ce3db4e8f
AWADREE <62517784+AWADREE@users.noreply.github.com>
2021-11-11 19:13:30 +0200

    added dash effect
    added dash effect and color change when play gets hit

--- Stone Game/Assets/Scripts/PlayerMovementController.cs
+++ Stone Game/Assets/Scripts/PlayerMovementController.cs
@@ -46,6 +46,7 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] float groundBoxXScal;
 
     [SerializeField] bool canMove = true; //for debugging
+    [SerializeField] bool isAlive = true; //for debugging
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         if(m_Started)
@@ -66,62 +67,61 @@ public class PlayerMovementController : MonoBehaviour
 	
 	void Update ()
     {
-
-        if(canMove)
+        
+        if(isAlive)
         {
 
-            //------------------------flip player
-            // moveSpeed = playerStats.GetMoveSpeed();
-
-            GetInput();
-            CheckIfFlipIsNeeded();
-        
-            //-----------------------------------------
-    
-
-            Vector2 v2GroundedBoxCheckPosition = (Vector2)transform.position + new Vector2(0, groundBoxYPos);
-            Vector2 v2GroundedBoxCheckScale = (Vector2)transform.localScale + new Vector2(groundBoxXScal, 0);
-
-            bool bGrounded = Physics2D.OverlapBox(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, lmWalls);
-
-            fGroundedRemember -= Time.deltaTime;
-            if (bGrounded)
+            if(canMove)
             {
-                fGroundedRemember = fGroundedRememberTime;
-            }
+                //------------------------flip player
+                GetInput();
+                CheckIfFlipIsNeeded();
+                //-----------------------------------------
 
-            fJumpPressedRemember -= Time.deltaTime;
-            if (Input.GetButtonDown("Jump"))
-            {
-                fJumpPressedRemember = fJumpPressedRememberTime;
-            }
+                Vector2 v2GroundedBoxCheckPosition = (Vector2)transform.position + new Vector2(0, groundBoxYPos);
+                Vector2 v2GroundedBoxCheckScale = (Vector2)transform.localScale + new Vector2(groundBoxXScal, 0);
 
-            if (Input.GetButtonUp("Jump"))
-            {
-                if (rigid.velocity.y > 0)
+                bool bGrounded = Physics2D.OverlapBox(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, lmWalls);
+
+                fGroundedRemember -= Time.deltaTime;
+                if (bGrounded)
                 {
-                    rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * fCutJumpHeight);
+                    fGroundedRemember = fGroundedRememberTime;
                 }
+
+                fJumpPressedRemember -= Time.deltaTime;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    fJumpPressedRemember = fJumpPressedRememberTime;
+                }
+
+                if (Input.GetButtonUp("Jump"))
+                {
+                    if (rigid.velocity.y > 0)
+                    {
+                        rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * fCutJumpHeight);
+                    }
+                }
+
+                if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
+                {
+                    fJumpPressedRemember = 0;
+                    fGroundedRemember = 0;
+                    rigid.velocity = new Vector2(rigid.velocity.x, fJumpVelocity);
+                }
+
+                float fHorizontalVelocity = rigid.velocity.x;
+                fHorizontalVelocity += Input.GetAxisRaw("Horizontal");
+
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+                    fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenStopping, Time.deltaTime * 10f);
+                else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(fHorizontalVelocity))
+                    fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenTurning, Time.deltaTime * 10f);
+                else
+                    fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
+
+                rigid.velocity = new Vector2(fHorizontalVelocity, rigid.velocity.y);//added *fHorizontalAcceleration*time.delta time
             }
-
-            if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
-            {
-                fJumpPressedRemember = 0;
-                fGroundedRemember = 0;
-                rigid.velocity = new Vector2(rigid.velocity.x, fJumpVelocity);
-            }
-
-            float fHorizontalVelocity = rigid.velocity.x;
-            fHorizontalVelocity += Input.GetAxisRaw("Horizontal");
-
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
-                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenStopping, Time.deltaTime * 10f);
-            else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(fHorizontalVelocity))
-                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenTurning, Time.deltaTime * 10f);
-            else
-                fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
-
-            rigid.velocity = new Vector2(fHorizontalVelocity, rigid.velocity.y);//added *fHorizontalAcceleration*time.delta time
         }
     }
 
@@ -150,9 +150,15 @@ public class PlayerMovementController : MonoBehaviour
     }
     //---------------------------
 
+    //used when casting abillities
     public void StopMoving()
     {
         canMove = false;
+        rigid.velocity = new Vector2( 0, rigid.velocity.y);
+    }
+    public void PlayerDead()
+    {
+        isAlive = false;
         rigid.velocity = new Vector2( 0, rigid.velocity.y);
     }
     public void StartMoving()
