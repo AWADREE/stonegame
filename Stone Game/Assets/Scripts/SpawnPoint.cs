fecd6eb16776fa8ce453302f5bec5f19c9e795d8
AWADREE <62517784+AWADREE@users.noreply.github.com>
2021-11-14 02:06:13 +0200

    added Enemy Respawn
    added spawn points for enemies and respawn times

---
+++ Stone Game/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Enemy myEnemy;
+    [SerializeField] Enemy enemyPrefab;
+    [SerializeField] float respawnTime;
+    float timSinceEnemyDied;
+    bool myEnemyIsDead;
+    void Start()
+    {
+        myEnemy = GetComponentInChildren<Enemy>();
+        gameObject.name = myEnemy.name+"Spawn Point";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myEnemyIsDead)
+        {
+            timSinceEnemyDied+= Time.deltaTime;
+        }
+
+        if(timSinceEnemyDied>= respawnTime)
+        {
+            //Respawn enemy
+            Instantiate(enemyPrefab,transform.position,Quaternion.identity).transform.SetParent(gameObject.transform);
+            myEnemyIsDead = false;
+            timSinceEnemyDied= 0;
+            myEnemy = GetComponentInChildren<Enemy>();
+        }
+    }
+
+    public void EnemyIsDead()
+    {
+        myEnemyIsDead = true;
+    }
+}
